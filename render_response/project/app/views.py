07bee9cbb368dbbab152b1c3500fe54a351284c2
anamsayyed016 <anamsayyed58@gmail.com>
2025-04-03 14:32:36 +0530

    3aprdj

---
+++ render_response/project/app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render , redirect
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return redirect('/index/')
+
+def index(request):
+    return HttpResponse("hello through redirect")
