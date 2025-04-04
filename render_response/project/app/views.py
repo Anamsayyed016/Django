1a2dd656eabe219f7988fe8df6ed206eb363b00b
anamsayyed016 <anamsayyed58@gmail.com>
2025-04-04 14:53:55 +0530

    4april

--- render_response/project/app/views.py
+++ render_response/project/app/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render , redirect
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return redirect('/index/')
+    name,city='anam','bhopal'
+
+    # return redirect(f'/index/?name={name}&city={city}')
+    return redirect('/index/?name={}&city={}.format(name,city)')
 
 def index(request):
-    return HttpResponse("hello through redirect")
+    print(request.method)
+    print(request.GET)
+    x=request.GET.get('name')
+    y=request.GET.get('city')
+    print(x,y)
+    # return HttpResponse("hello through redirect")
