5a0dc16f8e7e07040d17af53b331a70e06e7db33
anamsayyed016 <anamsayyed58@gmail.com>
2025-04-12 15:24:29 +0530

    djn

--- dtl_filter/project/app/views.py
+++ dtl_filter/project/app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request,'home.html',{'value':4})
+# def home(request):
+#     return render(request,'home.html',{'value':4})
 
 def home(request):
-    return render(request,'home.html',{'value':'django'})
+    return render(request,'home.html',{'value':'django','age':22})
