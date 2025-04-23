ebf93f340c460e73a67ecf5801cebcc4b562a472
anamsayyed016 <anamsayyed58@gmail.com>
2025-04-23 17:08:01 +0530

    django

---
+++ multitable_modelinheritence/project/app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Basefield(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=50)
+    contact=models.IntegerField()
+
+class Student(Basefield):
+    
+    course=models.CharField()
+    rollno=models.IntegerField()
+    fees=models.IntegerField()
+    def __str__(self):
+        return self.name
+
+class Employee(Basefield):
+    
+    department=models.CharField(max_length=50)
+    emp_id=models.EmailField(max_length=50)
+    salary=models.IntegerField()
+
+class Client(models.Model):
+    
+    project=models.CharField(max_length=50)
+    billing_status=models=None
